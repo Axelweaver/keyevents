47b4e670093a4acba77cf64e230966d6d947eafb
Axel Tkacheff <aptkachev@rolftech.ru>
2023-07-27 20:29:52 +0300

    Add files

---
+++ js/index.js
@@ -0,0 +1,55 @@
+	var data = { 
+		// keyDownEvent: {},
+		// keyUpEvent: {},
+		// KeyPressEvent: {}
+	};
+	
+function show(event, type) {
+
+	var jsonOutputElement = document.getElementById('jsonOutput');
+
+	if(!event){
+		return;
+	}
+
+	var eventData =  {
+		charCode: event.charCode,
+		code: event.code,
+		key: event.key,
+		keyCode: event.keyCode,
+		which: event.which,
+		shiftKey: event.shiftKey,
+		type: event.type
+	};
+	
+	switch (type) {
+		case 'keydown':
+			data.keyDownEvent = eventData;
+		case 'keyup':
+			data.keyUpEvent = eventData;
+		case 'keypress':
+			data.KeyPressEvent = eventData;
+		default:
+			data.KeyPressEvent = eventData;
+	}
+	
+	jsonOutputElement.innerHTML = JSON.stringify(data, undefined, 4);
+}
+	
+document.addEventListener('keydown', function(event) {
+	console.log('keydown', event);
+	
+	show(event, 'keydown');
+});
+
+document.addEventListener('keyup', function(event) {
+	console.log('keyup', event);
+
+	show(event, 'keyup');
+});
+
+document.addEventListener('keypress', function(event) {
+	console.log('keypress', event);
+
+	show(event, 'keypress');
+});
